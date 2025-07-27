6888db6ab685ccf62445cede1d65f0b6a67c0563
Gabriellemga <gabriellemga@gmail.com>
2025-07-27 10:27:58 -0300

    Create Desafio_04_listas.js

---
+++ Logica_programação/Desafio_04_listas.js
@@ -0,0 +1,30 @@
+//1-Crie uma lista vazia, com o nome listaGenerica.//
+
+let listaGenerica = [];
+
+//2-Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.//
+
+let linguagensDeProgramacao = ['JavaScrpt', 'C', 'C++', 'Kotlin', 'Python'];
+
+//3-Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.//
+
+let linguagensDeProgramacao = ['JavaScrpt', 'C', 'C++', 'Kotlin', 'Python'];
+
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+console.log(linguagensDeProgramacao);
+
+//4-Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.//
+
+let listasNomes = ['Joao', 'Ana', 'Gustavo'];
+console.log(listasNomes[0]);
+
+//5-Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.//
+
+let listasNomes = ['Joao', 'Ana', 'Gustavo'];
+console.log(listasNomes[1]);
+
+//6-Crie uma lista com 3 nomes e exiba no console apenas o último elemento.//
+
+let listasNomes = ['Joao', 'Ana', 'Gustavo'];
+console.log(listasNomes[listasNomes.length - 1]);
